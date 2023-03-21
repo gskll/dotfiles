d18ba0d9c737bce6e307711cfd1fb9435c58a61b
gskll <gskll@protonmail.com>
2023-03-21 18:19:54 +0100

    update fugitive mappings

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -1,7 +1,6 @@
 local keymap = vim.keymap
 
-keymap.set("n", "<leader>G", ":vertical Git<CR>")
-keymap.set("n", "<leader>Gc", ":Git commit -m \"")
+keymap.set("n", "<leader>G", ":Git<CR>")
+keymap.set("n", "<leader>Gc", ':Git commit -m "')
 keymap.set("n", "<leader>Gp", ":Git push -u origin HEAD<CR>")
-keymap.set("n", "<leader>GS", ":Git stash push -m \"")
-
+keymap.set("n", "<leader>GS", ':Git stash push -m "')
