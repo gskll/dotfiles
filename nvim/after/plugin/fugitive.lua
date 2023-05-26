2b667c065bcae2075539b8f93b76a5e1a9c3dcc7
gskll <gskll@protonmail.com>
2023-05-26 19:17:50 +0200

    add shortcuts to git commit no-verify

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -1,7 +1,7 @@
 local keymap = vim.keymap
 
 keymap.set("n", "<leader>G", ":Git<CR>")
-keymap.set("n", "<leader>Gc", ':Git commit -m "')
+keymap.set("n", "<leader>Gc", ':Git commit --no-verify -m "')
 keymap.set("n", "<leader>Gp", ":Git push -u origin HEAD<CR>")
 keymap.set("n", "<leader>GS", ':Git stash push -m "')
 
