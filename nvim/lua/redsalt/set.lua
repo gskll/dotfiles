45b399893ea0373cec8d15beee339582209e41e4
gskll <gskll@protonmail.com>
2023-06-07 11:24:41 +0200

    change tabwidth to 4

--- nvim/lua/redsalt/set.lua
+++ nvim/lua/redsalt/set.lua
@@ -12,9 +12,9 @@ opt.background = "dark"
 
 opt.backspace = "2"
 
-opt.tabstop = 2
-opt.softtabstop = 2
-opt.shiftwidth = 2
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
 opt.expandtab = true
 
 opt.autoindent = true
