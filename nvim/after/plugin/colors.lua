145f29caf677ace5dbf7e535672665538f854ca5
gskll <gskll@protonmail.com>
2023-06-09 18:05:21 +0200

    disable typescript interface semantic highlighting

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -8,6 +8,12 @@ function ColorMyPencils(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+	-- disable typescript interface semantic highlighting
+	-- clash between treesitter and lsp semantic highlighting
+	-- treats the whole interface declaration as interface keyword
+	-- and has the whole line in red
+	vim.api.nvim_set_hl(0, "@lsp.type.interface.typescript", {})
 end
 
 ColorMyPencils()
