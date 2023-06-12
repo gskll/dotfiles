c2d9e423933d376afb739d802d49e4ca4a122bb9
gskll <gskll@protonmail.com>
2023-06-12 23:00:26 +0200

    configure lsp/cmp plugins for snippets

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -6,6 +6,23 @@ local lsp = require("lsp-zero").preset({
 })
 local lspconfig = require("lspconfig")
 
+local cmp = require("cmp")
+local cmp_action = require("lsp-zero").cmp_action()
+
+require("luasnip.loaders.from_vscode").lazy_load()
+require("luasnip.loaders.from_vscode").lazy_load({ paths = { "./snippets/angular" } })
+
+cmp.setup({
+	sources = {
+		{ name = "nvim_lsp" },
+		{ name = "luasnip" },
+	},
+	mapping = {
+		["<C-f>"] = cmp_action.luasnip_jump_forward(),
+		["<C-b>"] = cmp_action.luasnip_jump_backward(),
+	},
+})
+
 lsp.ensure_installed({
 	"tsserver",
 	"lua_ls",
