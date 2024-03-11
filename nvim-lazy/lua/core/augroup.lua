954d77dbcd329fab772bd189ae13fe7beec3bbbc
Andrew Gaskell <gskll@protonmail.com>
2024-03-11 18:27:12 +0100

    add autocmds for numbertoggle and startup

--- nvim-lazy/lua/core/augroup.lua
+++ nvim-lazy/lua/core/augroup.lua
@@ -36,3 +36,27 @@ autocmd("BufWritePre", {
 	pattern = "*",
 	command = "%s/\\s\\+$//e",
 })
+
+local numbertoggleGroup = augroup("numbertoggle", { clear = true })
+autocmd({ "BufEnter", "FocusGained", "InsertLeave", "CmdlineLeave", "WinEnter" }, {
+	group = numbertoggleGroup,
+	pattern = "*",
+	callback = function()
+		if vim.o.nu and vim.api.nvim_get_mode().mode ~= "i" then
+			vim.opt.relativenumber = true
+		end
+	end,
+})
+autocmd({ "BufLeave", "FocusLost", "InsertEnter", "CmdlineEnter", "WinLeave" }, {
+	group = numbertoggleGroup,
+	pattern = "*",
+	callback = function()
+		if vim.o.nu then
+			vim.opt.relativenumber = false
+			vim.cmd("redraw")
+		end
+	end,
+})
+
+local startupGroup = augroup("startup", { clear = true })
+autocmd("VimEnter", { group = startupGroup, pattern = "*", command = "Ex" })
