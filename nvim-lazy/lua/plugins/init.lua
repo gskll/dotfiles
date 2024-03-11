ec8e7d60e999468397b47a210a198d3d5057a794
Andrew Gaskell <gskll@protonmail.com>
2024-03-11 18:27:28 +0100

    add basic lazy plugins

--- nvim-lazy/lua/plugins/init.lua
+++ nvim-lazy/lua/plugins/init.lua
@@ -1,11 +1,67 @@
-return {
-	-- harpoon -- no config
-	-- cellular-automation
-	-- comment
-	-- numbertoggle
-	-- undotree
-	-- autopairs
-	-- autotag
+local plugins = {
+	"nvim-lua/plenary.nvim",
+	"tpope/vim-sleuth",
+	"theprimeagen/harpoon",
+	"eandrju/cellular-automaton.nvim",
+	"mbbill/undotree",
+	"windwp/nvim-ts-autotag",
+	"szw/vim-maximizer",
+	"laytan/cloak.nvim",
+
+	{
+		"ellisonleao/gruvbox.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("core.utils").set_colorscheme()
+		end,
+	},
+	{
+		"folke/tokyonight.nvim",
+		opts = {
+			style = "storm",
+			terminal_colors = true,
+			styles = {
+				comments = { italic = false },
+				keywords = { italic = false },
+				sidebars = "dark",
+				floats = "dark",
+			},
+		},
+	},
+
+	{
+		"nvim-treesitter/nvim-treesitter",
+		event = { "BufReadPost", "BufNewFile", "BufWritePre", "VeryLazy" },
+		cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
+		build = ":TSUpdate",
+		opts = function()
+			return require("plugins.configs.treesitter")
+		end,
+		config = function(_, opts)
+			require("nvim-treesitter.configs").setup(opts)
+		end,
+		dependencies = {
+			{ "nvim-treesitter/nvim-treesitter-textobjects" },
+		},
+	},
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+		event = { "BufReadPost", "BufNewFile", "BufWritePre" },
+		enabled = true,
+		opts = { mode = "cursor", max_lines = 3 },
+	},
+	{
+		"echasnovski/mini.nvim",
+		version = false,
+		config = function(_, opts)
+			require("mini.ai").setup({ n_lines = 500 })
+			require("mini.surround").setup({ n_lines = 500 })
+			require("mini.comment").setup()
+			require("mini.pairs").setup()
+		end,
+	},
+
 	-- vim vinegar - just replace?
 	-- vim - sleuth ??
 	--
