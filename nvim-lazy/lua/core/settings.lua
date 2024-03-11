f44e763b44b5f1881110e15d5ff280595e3caa4a
Andrew Gaskell <gskll@protonmail.com>
2024-03-11 23:00:46 +0100

    add blinking cursor to see active window

--- nvim-lazy/lua/core/settings.lua
+++ nvim-lazy/lua/core/settings.lua
@@ -4,8 +4,11 @@ local opt = vim.opt
 g.mapleader = " "
 g.netrw_keepdir = 1
 g.netrw_browse_split = 0
-vim.g.netrw_banner = 0
-vim.g.netrw_winsize = 25
+g.netrw_banner = 0
+g.netrw_winsize = 25
+
+opt.guicursor =
+	"n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
 
 opt.number = true
 opt.numberwidth = 5
