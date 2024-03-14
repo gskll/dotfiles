8380026141117155588df1c98659307b90dcacdf
Andrew Gaskell <gskll@protonmail.com>
2024-03-14 14:36:30 +0100

    add switch colorscheme function to zsh

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -6,10 +6,11 @@ end
 
 -- get lualine gruvbox theme
 local lualine_gruvbox = require("lualine.themes.gruvbox")
+local lualine_tokyonight = require("lualine.themes.tokyonight")
 
 -- configure lualine with modified theme
 lualine.setup({
 	options = {
-		theme = lualine_gruvbox,
+		theme = lualine_tokyonight,
 	},
 })
