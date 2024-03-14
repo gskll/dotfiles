d257330b7457eda9f52d8f5d0ab99b54c56c9c28
Andrew Gaskell <gskll@protonmail.com>
2024-03-14 17:57:59 +0100

    add lazy formatting plugins

--- nvim-lazy/lua/plugins/formatting.lua
+++ nvim-lazy/lua/plugins/formatting.lua
@@ -1,1 +1,35 @@
-return {}
+return {
+	{
+		"stevearc/conform.nvim",
+		dependencies = { "mason.nvim" },
+		lazy = true,
+		cmd = "ConformInfo",
+		keys = {
+			{
+				"<leader>cF",
+				function()
+					require("conform").format({ formatters = { "injected" } })
+				end,
+				mode = { "n", "v" },
+				desc = "Format Injected Langs",
+			},
+		},
+		opts = {
+			notify_on_error = false,
+			format_on_save = {
+				timeout_ms = 500,
+				lsp_fallback = true,
+			},
+			formatters_by_ft = {
+				lua = { "stylua" },
+				python = { "black" },
+				javascript = { { "prettierd", "prettier" } },
+				sh = { "shfmt" },
+				go = { "goimports", "gofmt" },
+			},
+			formatters = {
+				injected = { options = { ignore_errors = true } },
+			},
+		},
+	},
+}
