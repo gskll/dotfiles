6a7f66447e12953fe360ccea95f68857fb4c2d4c
Andrew Gaskell <gskll@protonmail.com>
2024-03-14 15:35:47 +0100

    revert lualine

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -6,11 +6,10 @@ end
 
 -- get lualine gruvbox theme
 local lualine_gruvbox = require("lualine.themes.gruvbox")
-local lualine_tokyonight = require("lualine.themes.tokyonight")
 
 -- configure lualine with modified theme
 lualine.setup({
 	options = {
-		theme = lualine_tokyonight,
+		theme = lualine_gruvbox,
 	},
 })
