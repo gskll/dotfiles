5815bb9bee71a5da00548f527c270e0eb131c253
Andrew Gaskell <gskll@protonmail.com>
2024-03-15 13:27:46 +0100

    remove lualsp config

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -62,17 +62,6 @@ return {
 			lua_ls = {
 				settings = {
 					Lua = {
-						runtime = { version = "LuaJIT" },
-						workspace = {
-							checkThirdParty = false,
-							library = {
-								"${3rd}/luv/library",
-								unpack(vim.api.nvim_get_runtime_file("", true)),
-							},
-						},
-						completion = {
-							callSnippet = "Replace",
-						},
 						diagnostics = {
 							disable = { "missing-fields" },
 							globals = { "vim", "it", "describe", "before_each", "after_each" },
