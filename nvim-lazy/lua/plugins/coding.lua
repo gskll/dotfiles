b3153fb1ac055e80eb988859493ee731fd790645
Andrew Gaskell <gskll@protonmail.com>
2024-03-15 11:41:28 +0100

    add mini plugin coding

--- nvim-lazy/lua/plugins/coding.lua
+++ nvim-lazy/lua/plugins/coding.lua
@@ -146,4 +146,60 @@ return {
 			end, "Stop nearest test")
 		end,
 	},
+	{
+		"echasnovski/mini.comment",
+		event = "VeryLazy",
+		dependencies = {
+			"JoosepAlviste/nvim-ts-context-commentstring",
+			opts = {
+				enable_autocmd = false,
+			},
+		},
+		opts = {
+			options = {
+				custom_commentstring = function()
+					return require("ts_context_commentstring.internal").calculate_commentstring()
+						or vim.bo.commentstring
+				end,
+			},
+		},
+	},
+	{
+		"echasnovski/mini.ai",
+		event = "VeryLazy",
+		opts = function()
+			local ai = require("mini.ai")
+			return {
+				n_lines = 500,
+				custom_textobjects = {
+					o = ai.gen_spec.treesitter({
+						a = { "@block.outer", "@conditional.outer", "@loop.outer" },
+						i = { "@block.inner", "@conditional.inner", "@loop.inner" },
+					}, {}),
+					f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }, {}),
+					c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }, {}),
+					t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" },
+				},
+			}
+		end,
+	},
+	{
+		"echasnovski/mini.pairs",
+		event = "VeryLazy",
+		keys = {
+			{
+				"<leader>up",
+				function()
+					vim.g.minipairs_disable = not vim.g.minipairs_disable
+				end,
+				desc = "Toggle auto pairs",
+			},
+		},
+		opts = {},
+	},
+	{
+		"echasnovski/mini.surround",
+		event = "VeryLazy",
+		opts = { n_lines = 50 },
+	},
 }
