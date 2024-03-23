d803cb530c9d525b2e8312aedb493e89999f700d
Andrew Gaskell <gskll@protonmail.com>
2024-03-23 22:34:20 +0100

    add netrw keepdir 1 explicit

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -2,6 +2,7 @@ local g = vim.g
 local opt = vim.opt
 
 g.mapleader = " "
+g.netrw_keepdir = 1
 
 opt.guicursor =
 	"n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
