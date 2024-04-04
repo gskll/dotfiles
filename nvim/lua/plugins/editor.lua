5063197776b94ede8079131d70da33598ed358e8
gskll <gskll@protonmail.com>
2024-04-04 15:12:43 +0200

    format ignore harpoon keys

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -183,70 +183,17 @@ return {
 		"ThePrimeagen/harpoon",
 		branch = "harpoon2",
 		dependencies = { "nvim-lua/plenary.nvim" },
+        -- stylua: ignore
 		keys = {
-			{
-				"<leader>a",
-				function()
-					require("harpoon"):list():append()
-				end,
-				"Append to harpoon list",
-			},
-			{
-				"<C-e>",
-				function()
-					require("harpoon").ui:toggle_quick_menu(require("harpoon"):list())
-				end,
-				"Toggle harpoon popup",
-			},
-			{
-				"<leader>1",
-				function()
-					require("harpoon"):list():select(1)
-				end,
-				"Select harpoon list item 1",
-			},
-			{
-				"<leader>2",
-				function()
-					require("harpoon"):list():select(2)
-				end,
-				"Select harpoon list item 2",
-			},
-			{
-				"<leader>3",
-				function()
-					require("harpoon"):list():select(3)
-				end,
-				"Select harpoon list item 3",
-			},
-			{
-				"<leader>4",
-				function()
-					require("harpoon"):list():select(4)
-				end,
-				"Select harpoon list item 4",
-			},
-			{
-				"<leader>5",
-				function()
-					require("harpoon"):list():select(5)
-				end,
-				"Select harpoon list item 5",
-			},
-			{
-				"[h",
-				function()
-					require("harpoon"):list():prev()
-				end,
-				"Previous harpoon list buffer",
-			},
-			{
-				"]h",
-				function()
-					require("harpoon"):list():next()
-				end,
-				"Next harpoon list buffer",
-			},
+			{ "<leader>a", function() require("harpoon"):list():append() end, "Append to harpoon list" },
+			{ "<C-e>", function() require("harpoon").ui:toggle_quick_menu(require("harpoon"):list()) end, "Toggle harpoon popup" },
+			{ "<leader>1", function() require("harpoon"):list():select(1) end, "Select harpoon list item 1" },
+			{ "<leader>2", function() require("harpoon"):list():select(2) end, "Select harpoon list item 2" },
+			{ "<leader>3", function() require("harpoon"):list():select(3) end, "Select harpoon list item 3" },
+			{ "<leader>4", function() require("harpoon"):list():select(4) end, "Select harpoon list item 4" },
+			{ "<leader>5", function() require("harpoon"):list():select(5) end, "Select harpoon list item 5" },
+			{ "[h", function() require("harpoon"):list():prev() end, "Previous harpoon list buffer" },
+			{ "]h", function() require("harpoon"):list():next() end, "Next harpoon list buffer" },
 		},
 		opts = {},
 	},
