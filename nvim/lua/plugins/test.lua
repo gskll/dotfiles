35b56c9b7e391890323f9f511d93f71e056fb575
gskll <gskll@protonmail.com>
2024-04-04 15:14:14 +0200

    update neotest

--- nvim/lua/plugins/test.lua
+++ nvim/lua/plugins/test.lua
@@ -1,7 +1,6 @@
 return {
 	{
 		"nvim-neotest/neotest",
-		keys = { "<leader>tt", "<leader>tf", "<leader>td", "<leader>tc", "<leader>ts" },
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			"nvim-neotest/nvim-nio",
@@ -13,34 +12,49 @@ return {
 			"olimorris/neotest-phpunit",
 		},
 		config = function()
-			local neotest = require("neotest")
-			neotest.setup({
+			require("neotest").setup({
 				adapters = {
-					require("neotest-go"),
 					require("neotest-python"),
 					require("neotest-phpunit"),
+					require("neotest-go")({
+						experimental = { test_table = true },
+						recursive_run = true,
+						args = { "-count=1", "-timeout=60s" },
+					}),
+				},
+				status = { virtual_text = true },
+				output = { open_on_run = true },
+				quickfix = {
+					open = function()
+						if require("lazy.plugins.trouble") ~= nil then
+							require("trouble").open({ mode = "quickfix", focus = false })
+						else
+							vim.cmd("copen")
+						end
+					end,
 				},
 			})
-
-			vim.keymap.set("n", "<leader>tt", neotest.run.run, { desc = "Run nearest test" })
-			vim.keymap.set("n", "<leader>tf", function()
-				neotest.run.run(vim.fn.expand("%"))
-			end, { desc = "Run current file" })
-			vim.keymap.set("n", "<leader>tc", function()
-				neotest.run.run(vim.fn.getcwd())
-			end, { desc = "Run current file" })
-			vim.keymap.set("n", "<leader>td", function()
-				neotest.run.run({ strategy = "dap" })
-			end, { desc = "Debug nearest test" })
-			vim.keymap.set("n", "<leader>tx", neotest.run.stop, { desc = "Neotest: Stop test run" })
-			vim.keymap.set("n", "<leader>ts", neotest.summary.toggle, { desc = "Neotest: Toggle summary panel" })
-			vim.keymap.set("n", "<leader>tv", neotest.output_panel.toggle, { desc = "Neotest: Toggle output panel" })
-			vim.keymap.set("n", "[n", function()
-				neotest.jump.prev({ status = "failed" })
-			end, { desc = "Neotest: Jump prev failed test" })
-			vim.keymap.set("n", "]n", function()
-				neotest.jump.next({ status = "failed" })
-			end, { desc = "Neotest: Jump next failed test" })
+			local neotest_ns = vim.api.nvim_create_namespace("neotest")
+			vim.diagnostic.config({
+				virtual_text = {
+					format = function(diagnostic)
+						local message =
+							diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
+						return message
+					end,
+				},
+			}, neotest_ns)
 		end,
+        -- stylua: ignore
+		keys = {
+			{ "<leader>tf", function() require("neotest").run.run(vim.fn.expand("%")) end, desc = "Neotest: Run File" },
+			{ "<leader>tc", function() require("neotest").run.run(vim.uv.cwd()) end, desc = "Neotest: Run All Test Files" },
+			{ "<leader>tt", function() require("neotest").run.run() end, desc = "Neotest: Run Nearest" },
+			{ "<leader>tl", function() require("neotest").run.run_last() end, desc = "Neotest: Run Last" },
+			{ "<leader>ts", function() require("neotest").summary.toggle() end, desc = "Neotest: Toggle Summary" },
+			{ "<leader>tu", function() require("neotest").output.open({ enter = true, auto_close = true }) end, desc = "Neotest: Show Output" },
+			{ "<leader>tU", function() require("neotest").output_panel.toggle() end, desc = "Neotest: Toggle Output Panel" },
+			{ "<leader>tS", function() require("neotest").run.stop() end, desc = "Neotest: Stop" },
+		},
 	},
 }
