86a1ad101907ec47d560a7492b9c73b51de28665
gskll <gskll@protonmail.com>
2024-04-16 18:26:58 +0200

    add tokyonight-day (tokyolight) colorscheme

--- nvim/lua/core/colors.lua
+++ nvim/lua/core/colors.lua
@@ -2,10 +2,12 @@ M = {}
 
 -- this should be changed through the command line to keep tmux/alacritty in sync
 -- see zsh function `switch_color_scheme`
-M.active = "tokyonight"
+M.active = "tokyolight"
 
 function M.gruvbox()
+	print("Setting colorscheme to gruvbox", "Normal")
 	vim.cmd.colorscheme("gruvbox")
+	vim.opt.background = "dark"
 
 	-- disable typescript interface semantic highlighting
 	-- clash between treesitter and lsp semantic highlighting
@@ -16,10 +18,17 @@ end
 
 function M.tokyonight()
 	vim.cmd.colorscheme("tokyonight")
+	vim.opt.background = "dark"
+end
+
+function M.tokyolight()
+	vim.cmd.colorscheme("tokyonight")
+	vim.opt.background = "light"
 end
 
 function M.kanagawa()
 	vim.cmd.colorscheme("kanagawa")
+	vim.opt.background = "dark"
 end
 
 return M
